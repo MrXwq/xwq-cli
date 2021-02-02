a78580a7f674df8a29c3755036494eedef9e9f22
xuwenqiang <xuwenqiang1@tal.com>
2021-02-02 20:59:05 +0800

    add

--- xwq-cli/src/layout/index.vue
+++ xwq-cli/src/layout/index.vue
@@ -5,30 +5,76 @@
  * @LineEnd: ----------------------------------------------
 -->
 <template>
-  <div>
-    <header>头部</header>
-    <router-view class="body" />
+  <div class="layout-box">
+    <header>
+      <div>
+        <span>博客酱</span>
+        <i class="iconfont iconxieboke"></i>
+      </div>
+      <el-menu
+        :default-active="activeIndex"
+        mode="horizontal"
+        @select="handleSelect"
+      >
+        <el-menu-item index="1">首页</el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">博客类型</template>
+          <el-menu-item index="2-1">学习</el-menu-item>
+          <el-submenu index="2-2">
+            <template slot="title">生活</template>
+            <el-menu-item index="2-4-1">家庭</el-menu-item>
+            <el-menu-item index="2-4-2">朋友</el-menu-item>
+            <el-menu-item index="2-4-3">自己</el-menu-item>
+          </el-submenu>
+          <el-menu-item index="2-3">工作</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="3" disabled>留言榜</el-menu-item>
+      </el-menu>
+      <user-info></user-info>
+    </header>
+    <section>
+      <aside>左侧</aside>
+      <router-view class="body" />
+      <aside>右侧</aside>
+    </section>
     <footer>地步</footer>
   </div>
 </template>
 <script>
 export default {
   data() {
-    return {};
+    return {
+      activeIndex: "1"
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  },
+  components: {
+    "user-info": () =>
+      import("@/components/modules/MainHeader/UserInfo/index.vue") // 用户信息相关
   }
 };
 </script>
 <style lang="scss" scoped>
-header {
-  position: fixed;
-  width: 100%;
-  top: 0;
-  box-shadow: 0 0 1px rgb(0 0 0 / 25%);
-  transition: background-color 0.3s ease-in-out;
-  background-color: #fff;
-  height: 40px;
-  padding: 10px 60px;
-  z-index: 20;
+.layout-box {
+  padding-top: 60px;
+  header {
+    display: flex;
+    position: fixed;
+    top: 0;
+    z-index: 20;
+    width: 100%;
+    box-sizing: border-box;
+    // box-shadow: 0 0 1px rgb(0 0 25%);
+    transition: background-color 0.3s ease-in-out;
+    background-color: #fff;
+    .el-menu {
+      width: 50%;
+    }
+  }
 }
 .body {
   position: static;
